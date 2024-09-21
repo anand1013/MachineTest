77339ec8f448724e8834f21de4058825f7b980c6
anand1013 <anandksanthosh@gmail.com>
2024-09-21 09:10:22 +0530

    feat : resp

--- src/layouts/AdvantageTestimonials.jsx
+++ src/layouts/AdvantageTestimonials.jsx
@@ -7,7 +7,7 @@ import cong from "../configuration";
 // TestimonialList Component
 const TestimonialList = ({ testimonials }) => {
   return (
-    <div className="col-span-2 mb-8">
+    <div className="mt-12 col-span-2 mb-8 ">
       {testimonials.map((testimonial, index) => (
         <Testimonial
           key={index}
@@ -25,50 +25,27 @@ const TestimonialList = ({ testimonials }) => {
 };
 
 const AdvantageTestimonials = () => {
-  const features = [
-    {
-      icon: "M42.8 28.293C40.3867 28.293 39.4 26.5863 40.6 24.493C41.2933 23.2796 40.88 21.733 39.6667 21.0396L37.36 19.7196C36.3067 19.093 34.9467 19.4663 34.32 20.5196L34.1733 20.773C32.9733 22.8663 31 22.8663 29.7867 20.773L29.64 20.5196C29.04 19.4663 27.68 19.093 26.6267 19.7196L24.32 21.0396C23.1067 21.733 22.6933 23.293 23.3867 24.5063C24.6 26.5863 23.6133 28.293 21.2 28.293C19.8133 28.293 18.6667 29.4263 18.6667 30.8263V33.173C18.6667 34.5596 19.8 35.7063 21.2 35.7063C23.6133 35.7063 24.6 37.413 23.3867 39.5063C22.6933 40.7196 23.1067 42.2663 24.32 42.9596L26.6267 44.2796C27.68 44.9063 29.04 44.533 29.6667 43.4796L29.8133 43.2263C31.0133 41.133 32.9867 41.133 34.2 43.2263L34.3467 43.4796C34.9733 44.533 36.3333 44.9063 37.3867 44.2796L39.6933 42.9596C40.9067 42.2663 41.32 40.7063 40.6267 39.5063C39.4133 37.413 40.4 35.7063 42.8133 35.7063C44.2 35.7063 45.3467 34.573 45.3467 33.173V30.8263C45.3333 29.4396 44.2 28.293 42.8 28.293ZM32 36.333C29.6133 36.333 27.6667 34.3863 27.6667 31.9996C27.6667 29.613 29.6133 27.6663 32 27.6663C34.3867 27.6663 36.3333 29.613 36.3333 31.9996C36.3333 34.3863 34.3867 36.333 32 36.333Z", // Replace with actual path data
-      title: "Operational Efficiency",
-      description: "Automate ordering, payment, and stock management.",
-    },
-    {
-      icon: "M43.88 30.8263V24.973C43.88 23.8797 43.0533 22.6397 42.0267 22.2263L34.6 19.1863C32.9333 18.5063 31.0533 18.5063 29.3867 19.1863L21.96 22.2263C20.9467 22.6397 20.12 23.8797 20.12 24.973V30.8263C20.12 37.3463 24.8533 43.453 31.32 45.2397C31.76 45.3597 32.24 45.3597 32.68 45.2397C39.1467 43.453 43.88 37.3463 43.88 30.8263ZM33 33.1597V36.6663C33 37.213 32.5467 37.6663 32 37.6663C31.4533 37.6663 31 37.213 31 36.6663V33.1597C29.6533 32.733 28.6667 31.4797 28.6667 29.9997C28.6667 28.1597 30.16 26.6663 32 26.6663C33.84 26.6663 35.3333 28.1597 35.3333 29.9997C35.3333 31.493 34.3467 32.733 33 33.1597Z", // Replace with actual path data
-      title: "Operational Efficiency",
-      description: "Automate ordering, payment, and stock management.",
-    },
-    {
-      icon: "M43.88 30.8263V24.973C43.88 23.8797 43.0533 22.6397 42.0267 22.2263L34.6 19.1863C32.9333 18.5063 31.0533 18.5063 29.3867 19.1863L21.96 22.2263C20.9467 22.6397 20.12 23.8797 20.12 24.973V30.8263C20.12 37.3463 24.8533 43.453 31.32 45.2397C31.76 45.3597 32.24 45.3597 32.68 45.2397C39.1467 43.453 43.88 37.3463 43.88 30.8263ZM33 33.1597V36.6663C33 37.213 32.5467 37.6663 32 37.6663C31.4533 37.6663 31 37.213 31 36.6663V33.1597C29.6533 32.733 28.6667 31.4797 28.6667 29.9997C28.6667 28.1597 30.16 26.6663 32 26.6663C33.84 26.6663 35.3333 28.1597 35.3333 29.9997C35.3333 31.493 34.3467 32.733 33 33.1597Z", // Replace with actual path data
-      title: "Operational Efficiency",
-      description: "Automate ordering, payment, and stock management.",
-    },
-    {
-      icon: "M43.88 30.8263V24.973C43.88 23.8797 43.0533 22.6397 42.0267 22.2263L34.6 19.1863C32.9333 18.5063 31.0533 18.5063 29.3867 19.1863L21.96 22.2263C20.9467 22.6397 20.12 23.8797 20.12 24.973V30.8263C20.12 37.3463 24.8533 43.453 31.32 45.2397C31.76 45.3597 32.24 45.3597 32.68 45.2397C39.1467 43.453 43.88 37.3463 43.88 30.8263ZM33 33.1597V36.6663C33 37.213 32.5467 37.6663 32 37.6663C31.4533 37.6663 31 37.213 31 36.6663V33.1597C29.6533 32.733 28.6667 31.4797 28.6667 29.9997C28.6667 28.1597 30.16 26.6663 32 26.6663C33.84 26.6663 35.3333 28.1597 35.3333 29.9997C35.3333 31.493 34.3467 32.733 33 33.1597Z", // Replace with actual path data
-      title: "Operational Efficiency",
-      description: "Automate ordering, payment, and stock management.",
-    },
-    {
-      icon: "M43.88 30.8263V24.973C43.88 23.8797 43.0533 22.6397 42.0267 22.2263L34.6 19.1863C32.9333 18.5063 31.0533 18.5063 29.3867 19.1863L21.96 22.2263C20.9467 22.6397 20.12 23.8797 20.12 24.973V30.8263C20.12 37.3463 24.8533 43.453 31.32 45.2397C31.76 45.3597 32.24 45.3597 32.68 45.2397C39.1467 43.453 43.88 37.3463 43.88 30.8263ZM33 33.1597V36.6663C33 37.213 32.5467 37.6663 32 37.6663C31.4533 37.6663 31 37.213 31 36.6663V33.1597C29.6533 32.733 28.6667 31.4797 28.6667 29.9997C28.6667 28.1597 30.16 26.6663 32 26.6663C33.84 26.6663 35.3333 28.1597 35.3333 29.9997C35.3333 31.493 34.3467 32.733 33 33.1597Z", // Replace with actual path data
-      title: "Operational Efficiency",
-      description: "Automate ordering, payment, and stock management.",
-    },
-    {
-      icon: "M43.88 30.8263V24.973C43.88 23.8797 43.0533 22.6397 42.0267 22.2263L34.6 19.1863C32.9333 18.5063 31.0533 18.5063 29.3867 19.1863L21.96 22.2263C20.9467 22.6397 20.12 23.8797 20.12 24.973V30.8263C20.12 37.3463 24.8533 43.453 31.32 45.2397C31.76 45.3597 32.24 45.3597 32.68 45.2397C39.1467 43.453 43.88 37.3463 43.88 30.8263ZM33 33.1597V36.6663C33 37.213 32.5467 37.6663 32 37.6663C31.4533 37.6663 31 37.213 31 36.6663V33.1597C29.6533 32.733 28.6667 31.4797 28.6667 29.9997C28.6667 28.1597 30.16 26.6663 32 26.6663C33.84 26.6663 35.3333 28.1597 35.3333 29.9997C35.3333 31.493 34.3467 32.733 33 33.1597Z", // Replace with actual path data
-      title: "Operational Efficiency",
-      description: "Automate ordering, payment, and stock management.",
-    },
-  ];
-
-  const [data, setData] = useState([]);
+  const [testimonial, setTestimonial] = useState([]);
+  const [card, setCard] = useState([]);
   const database = getDatabase(cong);
-  const collectionRef = ref(database, "testimonial");
+  const testimonialCollectionRef = ref(database, "testimonial");
+  const cardCollectionRef = ref(database, "featurecard");
 
   useEffect(() => {
     const fetchData = () => {
-      onValue(collectionRef, (snapshot) => {
+      onValue(testimonialCollectionRef, (snapshot) => {
         const dataItem = snapshot.val();
         if (dataItem) {
           const displayItem = Object.values(dataItem);
-          setData(displayItem);
+          setTestimonial(displayItem);
+        }
+      });
+
+      onValue(cardCollectionRef, (snapshot) => {
+        const dataItem = snapshot.val();
+        if (dataItem) {
+          const displayItem = Object.values(dataItem);
+          setCard(displayItem);
         }
       });
     };
@@ -77,21 +54,37 @@ const AdvantageTestimonials = () => {
 
   return (
     <section className="bg-white">
-      <div className="items-center max-w-screen-xl px-4 py-8 mx-auto lg:grid lg:grid-cols-4 lg:gap-16 xl:gap-24 lg:py-24 lg:px-6">
+      <div className="items-center max-w-screen-xl px-4 py-8 mx-auto lg:grid lg:grid-cols-2 gap-12">
         {/* Features Section */}
-        <div className="col-span-2 space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0">
-          {features.map((feature, index) => (
-            <FeatureCard
-              key={index}
-              icon={feature.icon}
-              title={feature.title}
-              description={feature.description}
-            />
-          ))}
+        <div>
+          <div className="text-left">
+            <h1 className="text-start max-w-3xl mb-4 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold">
+              Ease in the <br />
+              Hand
+            </h1>
+            <p className="mb-8 lg:text-xl font-medium text-gray-500">
+              Using our app in business, especially in the restaurant industry,{" "}
+              <br />
+              has many advantages. Here are some of them:
+            </p>
+          </div>
+
+          <div className="col-span-2 space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0">
+            {card.map((feature, index) => (
+              <FeatureCard
+                key={index}
+                icon={feature.icon}
+                title={feature.title}
+                description={feature.description}
+              />
+            ))}
+          </div>
         </div>
 
         {/* Testimonials Section */}
-        <TestimonialList testimonials={data} />
+        <div>
+          <TestimonialList testimonials={testimonial} />
+        </div>
       </div>
     </section>
   );
--- src/layouts/Title.jsx
+++ src/layouts/Title.jsx
@@ -32,110 +32,132 @@ const Title = () => {
 
   return (
     <section className="bg-white">
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      {/* Banner Section */}
-      {/* <section className="mr-auto place-self-center sm:col-span-12 lg:col-span-7 rounded-3xl bg-gradient-to-b from-black to-black/90">
-        <div className="h-72 ml-4 sm:ml-10 mt-10 sm:mt-20 flex flex-col">
-          <h5 className="text-left mt-4 sm:mt-18 text-slate-500 text-sm sm:text-lg font-normal">
-            All the features you need in one App for any kind of <br /> your business 🚀
-          </h5>
-          <h1 className="text-left mt-6 text-white max-w-xs sm:max-w-2xl mb-4 text-3xl sm:text-5xl font-semibold leading-none tracking-tight">
-            Manage your restaurant <br /> business so advanced
-          </h1>
-          <SliderButton
-            label="Get Started for Free"
-            className="w-3/4 sm:w-1/2 mt-6 text-white bg-orange-500"
-            icon={arrow}
-          />
-        </div>
-        <img className="mb-6 w-full sm:w-auto" src={banner} alt="Banner" />
-      </section> */}
-  
-      {/* Testimonials and Reviews Section */}
-      <section className="col-span-1 m-4 md:m-0">
-        <BarChart
-          data={data}
-          xAxisLabels={[
-            "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec",
-          ]}
-          xKey="month"
-          yKey="earnings"
-          barColor="#8FBFFF"
-          barWidth={10}
-          animationConfig={{
-            duration: 1500,
-            onLoad: { duration: 800 },
-          }}
-          axisStyles={{
-            xAxis: { tickLabels: { fontSize: 10, sm: { fontSize: 12 }, padding: 5 } },
-            yAxis: { tickLabels: { fontSize: 10, sm: { fontSize: 12 }, padding: 5 } },
-          }}
-        />
-  
-        <section className="flex flex-col sm:flex-row space-x-0 sm:space-x-4">
-          <div className="basis-full sm:basis-1/2 rounded-3xl">
-            <section className="bg-gray-50 mb-8">
-              <div
-                className="max-w-screen-xl px-4 py-8 mx-auto text-center rounded-2xl"
-                style={{ background: "#F2ECFF" }}
-              >
-                <SliderButton
-                  label="Rating Review"
-                  className="text-slate-950 bg-white"
-                  icon={rating}
-                />
-                <figure className="max-w-screen-md mx-auto">
-                  <blockquote className="mb-7 mt-6">
-                    <p className="text-base text-slate-500 text-left">
-                      Reviews of some of our app restaurant industry companies.
-                    </p>
-                  </blockquote>
-                  <RatingScore rating="4.9" />
-                </figure>
-              </div>
-            </section>
-          </div>
-  
-          <div className="basis-full sm:basis-1/2 rounded-3xl">
-            <section className="bg-gray-50 mb-8">
-              <div className="max-w-screen-xl px-4 py-8 mx-auto text-center rounded-2xl bg-black/90">
-                <SliderButton
-                  label="Rating Review"
-                  className="text-white bg-slate-900"
-                  icon={partner}
-                />
-                <figure className="max-w-screen-md mx-auto">
-                  <blockquote className="mb-7 mt-6">
-                    <p className="text-base text-white text-left">
-                      Reviews of some of our app restaurant industry companies.
-                    </p>
-                  </blockquote>
-                  <div className="pr-3 font-bold text-4xl sm:text-6xl text-white">
-                    50{" "}
-                    <span className="font-semibold text-xl sm:text-2xl text-white">
-                      {" "}
-                      Thousand
-                    </span>
-                  </div>
-                </figure>
-              </div>
-            </section>
-          </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 m-4">
+        {/* Banner Section */}
+        <section className="col-span-1 m-4 md:m-0 rounded-3xl bg-gradient-to-b from-black/90 to-black flex flex-col justify-between" >
+
+            <div className=" h-72 ml-4 sm:ml-10 mt-10 sm:mt-20 flex flex-col">
+              <h5 className="text-left mt-4 sm:mt-18 text-slate-500 text-sm sm:text-lg font-normal">
+                All the features you need in one App for any kind of <br /> your
+                business 🚀
+              </h5>
+              <h1 className="text-left mt-6 text-white max-w-xs sm:max-w-2xl mb-4 text-3xl sm:text-5xl font-semibold leading-none tracking-tight">
+                Manage your restaurant <br /> business so advanced
+              </h1>
+              <SliderButton
+                label="Get Started for Free"
+                className="w-3/4 sm:w-1/2 mt-6 text-white bg-orange-500"
+                icon={arrow}
+              />
+            </div>
+            <img className="mb-6 w-full sm:w-auto" src={banner} alt="Banner" />
+        
         </section>
-  
-        <Testimonial
-          text="Deliver a better customer experience and increase their operational efficiency."
-          author="Jane Doe"
-          position="CEO at Company"
-          image={rating}
-          fontSize="text-sm sm:text-lg"
-          color="text-red-500"
-          background="#FFF5EE"
-        />
-      </section>
-    </div>
-  </section>
-  
+
+        {/* Testimonials and Reviews Section */}
+        <section className="col-span-1 m-4 md:m-0">
+          <BarChart
+            data={data}
+            xAxisLabels={[
+              "jan",
+              "feb",
+              "mar",
+              "apr",
+              "may",
+              "jun",
+              "jul",
+              "aug",
+              "sep",
+              "oct",
+              "nov",
+              "dec",
+            ]}
+            xKey="month"
+            yKey="earnings"
+            barColor="#8FBFFF"
+            barWidth={10}
+            animationConfig={{
+              duration: 1500,
+              onLoad: { duration: 800 },
+            }}
+            axisStyles={{
+              xAxis: {
+                tickLabels: { fontSize: 10, sm: { fontSize: 12 }, padding: 5 },
+              },
+              yAxis: {
+                tickLabels: { fontSize: 10, sm: { fontSize: 12 }, padding: 5 },
+              },
+            }}
+          />
+
+          <section className="flex flex-col sm:flex-row space-x-0 sm:space-x-4">
+            <div className="basis-full sm:basis-1/2 rounded-3xl">
+              <section className="bg-gray-50 mb-8">
+                <div
+                  className="max-w-screen-xl px-4 py-8 mx-auto text-center rounded-2xl"
+                  style={{ background: "#F2ECFF" }}
+                >
+                  <SliderButton
+                    label="Rating Review"
+                    className="text-slate-950 bg-white"
+                    icon={rating}
+                  />
+                  <figure className="max-w-screen-md mx-auto">
+                    <blockquote className="mb-7 mt-6">
+                      <p className="text-base text-slate-500 text-left">
+                        Reviews of some of our app restaurant industry
+                        companies.
+                      </p>
+                    </blockquote>
+                    <RatingScore rating="4.9" />
+                  </figure>
+                </div>
+              </section>
+            </div>
+
+            <div className="basis-full sm:basis-1/2 rounded-3xl">
+              <section className="mb-8">
+                <div
+                  className="max-w-screen-xl px-4 py-8 mx-auto text-center rounded-2xl "
+                  style={{ background: "#6d6d6f" }}
+                >
+                  <SliderButton
+                    label="Rating Review"
+                    className="text-white bg-slate-900"
+                    icon={partner}
+                  />
+                  <figure className="max-w-screen-md mx-auto">
+                    <blockquote className="mb-7 mt-6">
+                      <p className="text-base text-white text-left">
+                        Reviews of some of our app restaurant industry
+                        companies.
+                      </p>
+                    </blockquote>
+                    <div className="pr-3 font-bold text-4xl sm:text-6xl text-white">
+                      50{" "}
+                      <span className="font-semibold text-xl sm:text-2xl text-white">
+                        {" "}
+                        Thousand
+                      </span>
+                    </div>
+                  </figure>
+                </div>
+              </section>
+            </div>
+          </section>
+
+          <Testimonial
+            text="Deliver a better customer experience and increase their operational efficiency."
+            author="Jane Doe"
+            position="CEO at Company"
+            image={rating}
+            fontSize="text-sm sm:text-lg"
+            color="text-red-500"
+            background="#FFF5EE"
+          />
+        </section>
+      </div>
+    </section>
   );
 };
 
